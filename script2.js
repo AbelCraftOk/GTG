a85df8df6f9afa654812b4d29dbf049eca1a79ed
AbelCraftOk <pedrogabrielojeda664@gmail.com>
2025-08-02 23:53:09 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -40,7 +40,7 @@ function enviarMensaje(planillaData) {
 
     const payload = { embeds: [embed] };
 
-    fetch(BOTinsCifrada, {
+    fetch(inspectoresActiven, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
